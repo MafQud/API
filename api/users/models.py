94b86966777d6c2afe3340f6343327b0fb5ee8c9
Osama Yasser <osamayasserr@gmail.com>
2022-04-16 22:46:50 +0200

    feat: create initial user model

--- api/users/models.py
+++ api/users/models.py
@@ -1,26 +1,34 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
-from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+
+from .validators import is_phone
 
 
 class User(AbstractUser):
-    """
-    Default custom user model for API.
-    If adding fields that need to be filled at user signup,
-    check forms.SignupForm and forms.SocialSignupForms accordingly.
-    """
+    class Meta:
+        db_table = "users"
+        verbose_name = "user"
+        verbose_name_plural = "users"
 
-    #: First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None  # type: ignore
     last_name = None  # type: ignore
 
-    def get_absolute_url(self):
-        """Get url for user's detail view.
+    full_name = models.CharField(max_length=256)
+    email = models.EmailField(null=True, blank=True)
+    username = models.CharField(max_length=10, unique=True, validators=[is_phone])
 
-        Returns:
-            str: URL for user detail.
+    id_exp_date = models.DateTimeField(null=True, blank=True)
+    id_photo_url = models.ImageField(upload_to="id-photos/")
 
-        """
+    firebase_token = models.CharField(max_length=256, unique=True, blank=True)
+
+    def renew_id(self, days: int = 365) -> None:
+        self.id_exp_date = timezone.now() + timezone.timedelta(days=days)
+
+    def get_absolute_url(self) -> str:
         return reverse("users:detail", kwargs={"username": self.username})
+
+    def __str__(self) -> str:
+        return self.username
