3aaeba601f55c0f7bf76ff90fd2b6cd0f1aa341b
Osama Yasser <osamayasserr@gmail.com>
2022-04-17 23:10:52 +0200

    feat: add locations app models to admin site

--- api/locations/admin.py
+++ api/locations/admin.py
@@ -1,1 +1,23 @@
-# Admin
+from django.contrib import admin
+
+from .models import City, Governorate, Location
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ("name_ar", "name_en", "gov")
+    list_filter = ("gov",)
+    search_fields = ("name_en", "name_ar")
+
+
+@admin.register(Governorate)
+class GovernorateAdmin(admin.ModelAdmin):
+    list_display = ("name_ar", "name_en")
+    search_fields = ("name_en", "name_ar")
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ("lon", "lat", "address", "gov", "city")
+    list_filter = ("gov", "city")
+    search_fields = ("address",)
