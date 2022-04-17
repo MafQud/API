27eed05815516f71994d33d6afc31373837c5342
Osama Ragab <osamaragab520@gmail.com>
2022-04-17 22:59:39 +0200

    feat: add cases app models to admin site

--- api/cases/admin.py
+++ api/cases/admin.py
@@ -1,3 +1,5 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from .models import Case, CaseDetails, CaseMatch, CasePhoto
+
+admin.site.register((Case, CaseDetails, CaseMatch, CasePhoto))
