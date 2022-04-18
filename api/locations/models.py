d6df20c70f55355c0eafea886224115194f0f006
Osama Ragab <osamaragab520@gmail.com>
2022-04-18 02:39:41 +0200

    fix: modify address to accpet null

--- api/locations/models.py
+++ api/locations/models.py
@@ -32,9 +32,9 @@ class City(models.Model):
 
 
 class Location(models.Model):
-    lon = models.DecimalField(max_digits=9, decimal_places=6, null=True)
-    lat = models.DecimalField(max_digits=8, decimal_places=6, null=True)
-    address = models.CharField(max_length=512, blank=True)
+    lon = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    lat = models.DecimalField(max_digits=8, decimal_places=6, null=True, blank=True)
+    address = models.CharField(max_length=512, null=True, blank=True)
 
     gov = models.ForeignKey(Governorate, on_delete=models.PROTECT)
     city = models.ForeignKey(City, on_delete=models.PROTECT)
