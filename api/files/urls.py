390f5193a3f8dbe9a36689ce61df2995d24b3e7f
Osama Yasser <osamayasserr@gmail.com>
2022-04-22 23:56:44 +0200

    feat: s3 direct upload

---
+++ api/files/urls.py
@@ -0,0 +1,47 @@
+from django.urls import include, path
+
+from api.files.views import (
+    FileDirectUploadFinishApi,
+    FileDirectUploadLocalApi,
+    FileDirectUploadStartApi,
+    FileStandardUploadApi,
+)
+
+urlpatterns = [
+    path(
+        "upload/",
+        include(
+            (
+                [
+                    path("standard/", FileStandardUploadApi.as_view(), name="standard"),
+                    path(
+                        "direct/",
+                        include(
+                            (
+                                [
+                                    path(
+                                        "start/",
+                                        FileDirectUploadStartApi.as_view(),
+                                        name="start",
+                                    ),
+                                    path(
+                                        "finish/",
+                                        FileDirectUploadFinishApi.as_view(),
+                                        name="finish",
+                                    ),
+                                    path(
+                                        "local/<str:file_id>/",
+                                        FileDirectUploadLocalApi.as_view(),
+                                        name="local",
+                                    ),
+                                ],
+                                "direct",
+                            )
+                        ),
+                    ),
+                ],
+                "upload",
+            )
+        ),
+    )
+]
