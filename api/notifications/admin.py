969453d8cb3665fdd33eecf6bc5f1a1c9f95dfff
Osama Yasser <osamayasserr@gmail.com>
2022-05-06 19:00:15 +0200

    feat: add notificaions in admin site

--- api/notifications/admin.py
+++ api/notifications/admin.py
@@ -1,1 +1,9 @@
-# Admin
+from django.contrib import admin
+
+from api.notifications.models import Notification
+
+
+@admin.register(Notification)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ("id", "level", "sent_to")
+    list_filter = ("level",)
