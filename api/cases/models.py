f5ec8cac1ae85829d4a2b896987ee1ce93aba6ae
Osama Ragab <osamaragab520@gmail.com>
2022-05-08 06:30:16 +0200

    refactor: cases app

--- api/cases/models.py
+++ api/cases/models.py
@@ -1,7 +1,6 @@
-import datetime
-
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from django_fsm import FSMField, transition
 
@@ -39,6 +38,7 @@ class Case(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     posted_at = models.DateTimeField(null=True, default=None, blank=True)
     is_active = models.BooleanField(default=False, editable=False)
+    # TODO thumbnail
 
     @property
     def photo_urls(self):
@@ -73,8 +73,9 @@ class Case(models.Model):
     def activate_again(self):
         self.is_active = True
 
+    # FIXME
     def publish(self):
-        self.posted_at = datetime.now()
+        self.posted_at = timezone.now()
 
 
 class CaseDetails(models.Model):
