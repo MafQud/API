eb519062df2b53dd7ead72bd694eaab3ce72503c
Osama Ragab <osamaragab520@gmail.com>
2022-05-08 06:31:49 +0200

    refactor: locations app

--- api/locations/selectors.py
+++ api/locations/selectors.py
@@ -1,11 +1,11 @@
-from typing import Iterable
+from django.db.models.query import QuerySet
 
 from .models import City, Governorate
 
 
-def list_governorate() -> Iterable[Governorate]:
+def list_governorate() -> QuerySet[Governorate]:
     return Governorate.objects.all()
 
 
-def list_cities() -> Iterable[City]:
+def list_cities() -> QuerySet[City]:
     return City.objects.all()
