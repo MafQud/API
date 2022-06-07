7a5e921e78a79ceda715d279a6ab1ad57186e065
Osama Ragab <54740178+OsamaRagab520@users.noreply.github.com>
2022-06-07 14:32:25 +0200

    feat: add case state update endpoints (#81)

--- api/cases/urls.py
+++ api/cases/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from .apis import (
+    CaseArchiveApi,
     CaseContactCreateApi,
     CaseContactUpdateApi,
+    CaseFinishApi,
     CaseListApi,
     CaseMatchListApi,
     CasePublishApi,
@@ -25,4 +27,6 @@ urlpatterns = [
     # path("<int:case_id>/update/", UpdateCaseApi.as_view(), name="update"),
     path("<int:case_id>/matches/", CaseMatchListApi.as_view(), name="matches"),
     path("<int:case_id>/publish/", CasePublishApi.as_view(), name="publish"),
+    path("<int:case_id>/finish/", CaseFinishApi.as_view(), name="finish"),
+    path("<int:case_id>/archive/", CaseArchiveApi.as_view(), name="archive"),
 ]
